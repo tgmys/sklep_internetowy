b743ea44538cbc4e34eb8241c3d23a3653265d69
tgmys <t.gmys@o2.pl>
2023-08-09 22:48:20 +0200

    Dodaj pliki projektów.

---
+++ sklep_internetowy.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace sklep_internetowy.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //test 
+        }
+    }
+}
---
+++ sklep_internetowy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sklep_internetowy.MvcApplication" Language="C#" %>
