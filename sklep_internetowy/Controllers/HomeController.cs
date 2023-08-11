202e851aabc8ea91f6423f70947141dc1b208b8c
tgmys <t.gmys@o2.pl>
2023-08-11 14:49:56 +0200

    Pierwszy test bazy danych + dodanie bootstrapa

--- sklep_internetowy/Controllers/HomeController.cs
+++ sklep_internetowy/Controllers/HomeController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using sklep_internetowy.DAL;
+using sklep_internetowy.Models;
 
 namespace sklep_internetowy.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private KursyContext db = new KursyContext();
         public ActionResult Index()
         {
-            //praca nad finkcjonalnością
+            Kategoria kategoria = new Kategoria { NazwaKategorii = "asp.net mvc", NazwaPlikuIkony = "aspNetMVC.png", OpisKategorii = "opis" };
+            db.Kategorie.Add(kategoria);
+            db.SaveChanges();
+          
             return View();
         }
     }
