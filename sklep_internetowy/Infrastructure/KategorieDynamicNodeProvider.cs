dac722e4f384565dfd41cdbf6ad612fbfb0730c2
tgmys <t.gmys@o2.pl>
2024-04-14 15:29:27 +0200

    Dodanie ścieżki lokalizacyjnej, wgranie biblioteki mvcSiteProvider

---
+++ sklep_internetowy/Infrastructure/KategorieDynamicNodeProvider.cs
@@ -0,0 +1,31 @@
+﻿using MvcSiteMapProvider;
+using sklep_internetowy.DAL;
+using sklep_internetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sklep_internetowy.Infrastructure
+{
+    public class KategorieDynamicNodeProvider :DynamicNodeProviderBase
+    {
+
+        private KursyContext db = new KursyContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach (Kategoria kat in db.Kategorie)
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = kat.NazwaKategorii;
+                node.Key = "Kategoria_" + kat.KategoriaId;            
+                node.RouteValues.Add("nazwaKategorii", kat.NazwaKategorii);
+                returnValue.Add(node);
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ sklep_internetowy/Infrastructure/KursySzczegolyDynamicNodeProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcSiteMapProvider;
+using sklep_internetowy.DAL;
+using sklep_internetowy.Models;
+
+namespace sklep_internetowy.Infrastructure
+{
+    public class KursySzczegolyDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private KursyContext db = new KursyContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+            foreach(Kurs kurs in db.Kursy )
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = kurs.TytulKursu;
+                node.Key = "Kurs_" + kurs.KursId;
+                node.ParentKey = "Kategoria_" + kurs.KategoriaID;
+                node.RouteValues.Add("id", kurs.KursId);
+                returnValue.Add(node);
+            }
+
+            return returnValue;
+        }
+
+    }
+}
