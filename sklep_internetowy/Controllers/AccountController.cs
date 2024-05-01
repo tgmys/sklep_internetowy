f29898c4987f5817564df5456f95f832d6c2ee38
tgmys <t.gmys@o2.pl>
2024-05-01 18:20:41 +0200

    Dodanie formularzy logowania i rejestracji użytkowników

---
+++ sklep_internetowy/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using sklep_internetowy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sklep_internetowy.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+
+                return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register (RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ sklep_internetowy/ViewModels/AccountViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace sklep_internetowy.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Musisz wprowadzić e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Musisz wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name ="Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage ="{0} musi mieć co najmniej {2} znaków", MinimumLength =6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdż Hasło")]
+        [Compare("Password", ErrorMessage ="Hasło i potwierdzenie hasła nie są identyczne")]
+        public string ConfirmPassword { get; set; }
+    }
+}
